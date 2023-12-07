6db9450da89675b3d9576bf0d43cefaa754c8efd
mickaelPires <93197821+mickaelPires@users.noreply.github.com>
2023-12-07 20:21:22 +0100

    linechart

--- lexiamessureschart/src/App.js
+++ lexiamessureschart/src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./pages/Home";
+import LineChart from "./composant/LineChart";
 
 function App() {
   return (
@@ -18,6 +20,8 @@ function App() {
           Learn React
         </a>
       </header>
+      <Home />
+      <LineChart />
     </div>
   );
 }
